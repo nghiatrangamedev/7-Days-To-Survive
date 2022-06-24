fa03baa8538c0af5dd81dcf1d42b4ca24ea76026
nghiatran98 <nghiatran19980613@gmail.com>
2022-06-24 22:00:46 +0700

    Update 0.4
    - Add Spawn Enemy features

--- Assets/Scripts/Base.cs
+++ Assets/Scripts/Base.cs
@@ -25,18 +25,6 @@ public class Base : MonoBehaviour
         get { return _heath; }
     }
 
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
-
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Hand")
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,23 +5,37 @@ using TMPro;
 
 public class GameManager : MonoBehaviour
 {
+    [SerializeField] GameObject _enemyPrefabs;
     [SerializeField] TextMeshProUGUI _heathText;
     [SerializeField] TextMeshProUGUI _ammoText;
+    [SerializeField] GameObject _gameOver;
+    [SerializeField] GameObject _youWin;
     [SerializeField] GameObject _base;
 
     Base _baseScript;
     PlayerController _playerController;
+
+    int _maximumEnemies = 20;
+    int _enemiesSpawned = 0;
+    int _posX = -17;
+
+    float _rangeY = 3.7f;
+    float _startTime = 2.0f;
+    float _timeRate = 1.0f;
 
     // Start is called before the first frame update
     void Start()
     {
         _baseScript = _base.GetComponent<Base>();
         _playerController = GameObject.Find("Player").GetComponent<PlayerController>();
+
+        InvokeRepeating("SpawnEnemy", _startTime, _timeRate);
     }
 
     // Update is called once per frame
     void Update()
     {
+        CheckWinCondition();
         GameOver();
         DisplayHeath();
         DisplayAmmo();
@@ -32,7 +46,7 @@ public class GameManager : MonoBehaviour
         if (_baseScript.Heath == 0)
         {
             Destroy(_base);
-            Debug.Log("Game Over");
+            _gameOver.SetActive(true);
         }
     }
 
@@ -44,5 +58,30 @@ public class GameManager : MonoBehaviour
     void DisplayAmmo()
     {
         _ammoText.SetText("Ammo: " + _playerController.AmmoTotal);
+    }
+
+    void SpawnEnemy()
+    {
+        if (_enemiesSpawned <= _maximumEnemies)
+        {
+            Instantiate(_enemyPrefabs, RandomEnemyPos(), _enemyPrefabs.transform.rotation);
+            _enemiesSpawned++;
+        }
+    }
+
+    Vector2 RandomEnemyPos()
+    {
+        Vector2 result;
+        float randomPosY = Random.Range(-_rangeY, _rangeY);
+        result = new Vector2(_posX, randomPosY);
+        return result;
+    }
+
+    void CheckWinCondition()
+    {
+        if (_enemiesSpawned >= _maximumEnemies && GameObject.FindGameObjectsWithTag("Enemy").Length == 0)
+        {
+            _youWin.SetActive(true);
+        }
     }
 }
